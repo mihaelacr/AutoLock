86ac3d34e764ddcc182297063bb5dd71ce5cbd92
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-02-20 08:51:08 +0000

    Print user friendly message when a they terminate the program from keyboard (Control-c)

--- getWebcamData.py
+++ getWebcamData.py
@@ -1,15 +1,20 @@
 import argparse
 import cv
 import time
+import signal
+import sys
 
 from faceRecognition import *
 import lockScreen
 import batteryStatus
 
-DEFAULT_TIME_UNTIL_LOCK = 5
 
-parser = argparse.ArgumentParser(description=("Automatically lock your screen when not in"
-                                              "in range"))
+DEFAULT_TIME_UNTIL_LOCK = 5
+WINDOW_NAME = "AutoLock"
+TIME_BETWEEN_FACE_CHECKS = 1
+
+parser = argparse.ArgumentParser(description=("Automatically lock your screen "
+                                              "when not inin range"))
 parser.add_argument("-timeUntilLock", type=int, default=DEFAULT_TIME_UNTIL_LOCK,
                       help=("time in seconds since the last time a face is detected"
                            "to the time the screen is locked"))
@@ -20,17 +25,26 @@ parser.add_argument("--runWithDischargingBattery",
                             "when the battery is discharging"))
 
 
-WINDOW_NAME = "AutoLock"
-TIME_BETWEEN_FACE_CHECKS = 1
-
 args = parser.parse_args()
 displayCam = args.displayWebcam
 timeUntilLock = args.timeUntilLock
 runWithDischargingBattery = args.runWithDischargingBattery
 
+
+# When user presses Control-C, gracefully exit program, without
+def signal_handler(signal, frame):
+  print 'You pressed Ctrl+C!'
+  print "AutoLock will terminate."
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+
 def getCameraCapture():
   # -1 is supposed to detected any webcam connected to the device
   return cv.CaptureFromCAM(-1)
+
 
 def lockWhenFaceNotDetected(timeUntilLock, display=False):
   if display:
@@ -39,6 +53,7 @@ def lockWhenFaceNotDetected(timeUntilLock, display=False):
   capture = getCameraCapture()
   lastTimeDetected = time.time()
   lastTimeChecked = time.time()
+
   while True:
     # Unless the user specified otherwise, do not record while machine is not
     # not charging
@@ -50,6 +65,8 @@ def lockWhenFaceNotDetected(timeUntilLock, display=False):
       frame = cv.QueryFrame(capture)
       if display:
         cv.ShowImage(WINDOW_NAME, frame)
+        if cv.WaitKey(5) == 27:
+          break
       faces = getFaces(frame)
       if faces:
         lastTimeDetected = currentTime
@@ -58,13 +75,11 @@ def lockWhenFaceNotDetected(timeUntilLock, display=False):
           print "no face found, locking screen"
           lockScreen.lockScreen()
       drawFaces(faces, frame)
-      cv.WaitKey(10)
-      # time.sleep(0.5)
 
 
 def main():
   global timeUntilLock
-  if batteryStatus.isCharging or runWithDischargingBattery:
+  if batteryStatus.isCharging() or runWithDischargingBattery:
     if runWithDischargingBattery:
       print "You choose to run AutoLock with your laptop not plugged in"
       print "Be aware of your battery"
