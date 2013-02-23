4c3438cb617c9b654b7b4079dc7265a827401a4b
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-02-23 00:22:48 +0000

    Added slock as backup in case gnome fails

--- lockScreen.py
+++ lockScreen.py
@@ -1,7 +1,11 @@
 import subprocess
 
 def lockScreen():
-  subprocess.call(["gnome-screensaver-command", "--lock"])
+  try:
+    subprocess.check_call(["gnome-screensaver-command", "--lock"])
+  except subprocess.CalledProcessErrror:
+    subprocess.call(["slock"])
+
 
 if __name__ == '__main__':
   lockScreen()
