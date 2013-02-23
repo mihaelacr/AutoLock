9e47f2fe09ea8386137c01f5f28807a71afed615
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-02-23 00:33:04 +0000

    TODO and catch OSError in lockScreen

--- lockScreen.py
+++ lockScreen.py
@@ -3,7 +3,9 @@ import subprocess
 def lockScreen():
   try:
     subprocess.check_call(["gnome-screensaver-command", "--lock"])
-  except subprocess.CalledProcessErrror:
+  # TODO there should be no need for OS error but it seems
+  # to raise it when the command above is altered
+  except (subprocess.CalledProcessError, OSError):
     subprocess.call(["slock"])
 
 
