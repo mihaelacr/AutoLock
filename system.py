fcf99feefaab72109d151bff00e8366d35f1b605
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-05-26 10:21:58 -0700

    Added support for getting the battery status in Linux

--- system.py
+++ system.py
@@ -5,33 +5,57 @@ MAC = "darwin"
 LINUX = "linux"
 
 
+PATH_TO_BATTERY_INFO_LINUX = "/proc/acpi/battery/"
+
+CHARGING_KEY_LINUX = "charging state"
+DISCHARGING_LINUX = "discharging"
+
 class UnsupportedOsException(Exception):
 
   def __init__(self, os):
-    self.message = "AutoLock: Unsupported os %s" % os
+    self.message = "AutoLock: Unsupported OS %s" % os
 
-class OsHandler(object):
+def lockScreen(self):
+  os = getOS()
+  if os == LINUX:
+    try:
+      subprocess.check_call(["gnome-screensaver-command", "--lock"])
+    except (subprocess.CalledProcessError, OSError):
+      # TODO what happens is slock is not installed?
+      subprocess.call(["slock"])
+  elif os == MAC:
+    try:
+      subprocess.check_call(["/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession", "-suspend"])
+    except (subprocess.CalledProcessError, OSError):
+      pass
 
-  def __init__(self):
-    self.os = getOS()
 
-  def lockScreen(self):
-    if self.os == LINUX:
-      try:
-        subprocess.check_call(["gnome-screensaver-command", "--lock"])
-      except (subprocess.CalledProcessError, OSError):
-       # TODO what happens is slock is not installed?
-       subprocess.call(["slock"])
-    elif self.os == MAC:
-      try:
-        subprocess.check_call(["/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession", "-suspend"])
-      except (subprocess.CalledProcessError, OSError):
-         pass
+def parseBatteryStatusFileLinux(statFile=None):
+  def getBatteryStatusFile():
+    ls_output = subprocess.check_output(["ls",       PATH_TO_BATTERY_INFO_LINUX])
+    files = ls_output.split()
+    return PATH_TO_BATTERY_INFO_LINUX + files[0] + "/state"
+
+  batteryStatus = {}
+  if not statFile:
+    statFile = getBatteryStatusFile()
+  for line in open(statFile):
+    key, value = line.split(":")[0:2]
+    batteryStatus[key] = value.strip()
+  return batteryStatus
+
+
+def isCharging():
+  os = getOS()
+  if os == LINUX:
+    batteryStatus = parseBatteryStatusFileLinux()
+    return batteryStatus[CHARGING_KEY_LINUX] != DISCHARGING_LINUX
+  elif os == MAC:
+    pass
 
 
 def isSupportedOS(os):
   return os in [MAC, LINUX]
-
 
 def getOS():
   os = platform.system().lower()
